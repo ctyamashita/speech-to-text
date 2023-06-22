6e962d4374ab926dff7d3d276ff1bc124b1a3f85
Celso Yamashita <ctyamashita@hotmail.com>
2023-06-23 00:26:00 +0900

    testing google sheet api

--- gsheets.js
+++ gsheets.js
@@ -37,48 +37,49 @@ function makeApiCall() {
   });
 }
 
-function initClient() {
-  var API_KEY = 'AIzaSyCZTtWUFHqiPqJT-sELX9EAWF6dr5A7MoY';  // TODO: Update placeholder with desired API key.
+// function initClient() {
+//   var API_KEY = 'AIzaSyCZTtWUFHqiPqJT-sELX9EAWF6dr5A7MoY';  // TODO: Update placeholder with desired API key.
 
-  var CLIENT_ID = '74111767360-3e6qt101ihkhp8pvoc570efmlsg3o9rl.apps.googleusercontent.com';  // TODO: Update placeholder with desired client ID.
+//   var CLIENT_ID = '74111767360-3e6qt101ihkhp8pvoc570efmlsg3o9rl.apps.googleusercontent.com';  // TODO: Update placeholder with desired client ID.
 
-  // TODO: Authorize using one of the following scopes:
-  //   'https://www.googleapis.com/auth/drive'
-  //   'https://www.googleapis.com/auth/drive.file'
-  //   'https://www.googleapis.com/auth/drive.readonly'
-  //   'https://www.googleapis.com/auth/spreadsheets'
-  //   'https://www.googleapis.com/auth/spreadsheets.readonly'
-  var SCOPE = 'https://www.googleapis.com/auth/drive.file';
+//   // TODO: Authorize using one of the following scopes:
+//   //   'https://www.googleapis.com/auth/drive'
+//   //   'https://www.googleapis.com/auth/drive.file'
+//   //   'https://www.googleapis.com/auth/drive.readonly'
+//   //   'https://www.googleapis.com/auth/spreadsheets'
+//   //   'https://www.googleapis.com/auth/spreadsheets.readonly'
+//   var SCOPE = 'https://www.googleapis.com/auth/drive.file';
 
-  gapi.client.init({
-    'apiKey': API_KEY,
-    'clientId': CLIENT_ID,
-    'scope': SCOPE,
-    'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-  }).then(function() {
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
-    updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-  });
-}
+//   gapi.client.init({
+//     'apiKey': API_KEY,
+//     'clientId': CLIENT_ID,
+//     'scope': SCOPE,
+//     'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+//   }).then(function() {
+//     gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
+//     updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+//   });
+// }
 
-function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
-}
+// function handleClientLoad() {
+//   gapi.load('client:auth2', initClient);
+// }
 
-function updateSignInStatus(isSignedIn) {
-  if (isSignedIn) {
-    makeApiCall();
-  }
-}
+// function updateSignInStatus(isSignedIn) {
+//   if (isSignedIn) {
+//     makeApiCall();
+//   }
+// }
 
-function handleSignInClick(event) {
-  gapi.auth2.getAuthInstance().signIn();
-}
+// function handleSignInClick(event) {
+//   gapi.auth2.getAuthInstance().signIn();
+// }
 
-function handleSignOutClick(event) {
-  gapi.auth2.getAuthInstance().signOut();
-}
+// function handleSignOutClick(event) {
+//   gapi.auth2.getAuthInstance().signOut();
+// }
 
 globalThis.handleCredentialResponse = async (response) => {
   console.log(response);
+  makeApiCall();
 }
