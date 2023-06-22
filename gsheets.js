3a815daf0d880710db44f02448d65f5efa34c6ba
Celso Yamashita <ctyamashita@hotmail.com>
2023-06-23 01:12:01 +0900

    test

--- gsheets.js
+++ gsheets.js
@@ -28,55 +28,9 @@ function makeApiCall() {
     dateTimeRenderOption: '',  // TODO: Update placeholder value.
   };
 
-  var request = gapi.client.sheets.spreadsheets.values.get(params);
-  request.then(function(response) {
-    // TODO: Change code below to process the `response` object:
-    console.log(response.result);
-  }, function(reason) {
-    console.error('error: ' + reason.result.error.message);
-  });
-}
-
-function initClient() {
-  var API_KEY = 'AIzaSyCZTtWUFHqiPqJT-sELX9EAWF6dr5A7MoY';  // TODO: Update placeholder with desired API key.
-
-  var CLIENT_ID = '74111767360-3e6qt101ihkhp8pvoc570efmlsg3o9rl.apps.googleusercontent.com';  // TODO: Update placeholder with desired client ID.
-
-  // TODO: Authorize using one of the following scopes:
-  //   'https://www.googleapis.com/auth/drive'
-  //   'https://www.googleapis.com/auth/drive.file'
-  //   'https://www.googleapis.com/auth/drive.readonly'
-  //   'https://www.googleapis.com/auth/spreadsheets'
-  //   'https://www.googleapis.com/auth/spreadsheets.readonly'
-  var SCOPE = 'https://www.googleapis.com/auth/drive.file';
-
-  gapi.client.init({
-    'apiKey': API_KEY,
-    'clientId': CLIENT_ID,
-    'scope': SCOPE,
-    'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-  }).then(function() {
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
-    updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-  });
-}
-
-function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
-}
-
-function updateSignInStatus(isSignedIn) {
-  if (isSignedIn) {
-    makeApiCall();
-  }
-}
-
-function handleSignInClick(event) {
-  gapi.auth2.getAuthInstance().signIn();
-}
-
-function handleSignOutClick(event) {
-  gapi.auth2.getAuthInstance().signOut();
+  fetch(`https://sheets.googleapis.com/v4/spreadsheets/${params.spreadsheetId}/values/${params.range}`).then(response=> response.json()).then(data => {
+    console.log(data);
+  })
 }
 
 globalThis.handleCredentialResponse = async (response) => {
